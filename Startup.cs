3cddec1c912920e514e5ce4e6a0173149e5756ee
Susan <szhou089@gmail.com>
2022-02-28 17:35:23 -0800

    Reverting back to 5.0

--- Startup.cs
+++ Startup.cs
@@ -25,15 +25,15 @@ namespace GlobalATM
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MyContext>(dbCtxOptions =>
-        {
-            dbCtxOptions.UseMySql(Configuration["DBInfo:ConnectionString"], mySqlOptions => mySqlOptions.EnableRetryOnFailure());
-        });
+            services.AddDbContext<MyContext>(options => options.UseMySql(
+                    Configuration["DBInfo:ConnectionString"],
+                    ServerVersion.FromString("8.0.23-mysql")));
+
+            // to access session directly from view, corresponds with: @using Microsoft.AspNetCore.Http in Views/_ViewImports.cshtml
             services.AddHttpContextAccessor();
             services.AddSession();
-            services.AddDbContext<MyContext>(options => options.UseMySql (Configuration["DBInfo:ConnectionString"]));
             services.AddMvc(options => options.EnableEndpointRouting = false);
-            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
