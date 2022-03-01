31617b3e990245ce27377ebdde0b2116beb0b07f
Shirley Li <lishirley@me.com>
2022-02-28 17:42:28 -0800

    added send receipt method and Nick's changes

---
+++ Models/Conversion.cs
@@ -0,0 +1,25 @@
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace GlobalATM.Models
+{
+    public class ConversionRaw
+    {
+        public double Amount {get; set;}
+        public string Base_Currency_Code {get; set;}
+        public string Base_Currency_Name {get;set;}
+
+        public double Rate {get; set;}
+        public double Rate_For_Amount {get;set;}
+
+    }
+    public class ConversionObject
+    {
+        public ConversionRaw[] conversion {get;set;}
+    }
+    // var conversionObject = JsonConvert.DeserializeObject<ConversionObject>(json);
+} 
