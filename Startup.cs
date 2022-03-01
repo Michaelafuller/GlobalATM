46dc87b0bcb005cc1a3b6d3c5e7ff1cf1fe666d5
Susan <szhou089@gmail.com>
2022-02-28 17:57:59 -0800

    Resolving Conflict

--- Startup.cs
+++ Startup.cs
@@ -33,30 +33,29 @@ namespace GlobalATM
             services.AddHttpContextAccessor();
             services.AddSession();
             services.AddMvc(options => options.EnableEndpointRouting = false);
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Home/Error");
-        }
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
-        // css, js, and image files can now be added to wwwroot folder
-        app.UseStaticFiles();
-        app.UseSession();
-        app.UseMvc(routes =>
-        {
-            routes.MapRoute(
-                name: "default",
-                template: "{controller=Home}/{action=Index}/{id?}");
-        });
+            // css, js, and image files can now be added to wwwroot folder
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
